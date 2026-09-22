9bcc71fb54f7ce2aef0cc6da7cfdb5c1a5610c8d
tenixagi <tenixagi@gmail.com>
2026-09-22 22:54:04 +0530

    new

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -14,12 +14,12 @@ export const SOCIAL_LINKS = {
   // Tracking/Analytics
   DEX_SCREENER: "https://dexscreener.com",
   SOLSCAN: "https://solscan.io",
-  PUMP_FUN: "https://pump.fun/coin/Ce1i2XxZrScNnJDDQiiZqSMvMT44UYDJgHeiNpjYpump",
+  PUMP_FUN: "https://pump.fun/coin/G5gMsrWSmB9W3mAZZFGmGTYghSj4ZPd8FS2KAvkipump",
 } as const;
 
 //CA
-export const CONTRACT_ADDRESS = "Ce1i2XxZrScNnJDDQiiZqSMvMT44UYDJgHeiNpjYpump";
-export const CONTRACT_ADDRESS_SHORT = "Ce1i2XxZrScNnJDDQiiZqSMvMT44UYDJgHeiNpjYpump";
+export const CONTRACT_ADDRESS = "G5gMsrWSmB9W3mAZZFGmGTYghSj4ZPd8FS2KAvkipump";
+export const CONTRACT_ADDRESS_SHORT = "G5gMsrWSmB9W3mAZZFGmGTYghSj4ZPd8FS2KAvkipump";
 
 // Type for social link keys
 export type SocialLinkKey = keyof typeof SOCIAL_LINKS;
