2b543eb0669acad08882e2026cc2263677558b80
tenixagi <tenixagi@gmail.com>
2026-09-23 17:38:50 +0530

    new

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -14,12 +14,12 @@ export const SOCIAL_LINKS = {
   // Tracking/Analytics
   DEX_SCREENER: "https://dexscreener.com",
   SOLSCAN: "https://solscan.io",
-  PUMP_FUN: "https://pump.fun/coin/NRJFKBGRo5qyUpTZ3rof6Sj4t79TW6sr3XMvp1kpump",
+  PUMP_FUN: "https://pump.fun/coin/",
 } as const;
 
 //CA
-export const CONTRACT_ADDRESS = "NRJFKBGRo5qyUpTZ3rof6Sj4t79TW6sr3XMvp1kpump";
-export const CONTRACT_ADDRESS_SHORT = "NRJFKBGRo5qyUpTZ3rof6Sj4t79TW6sr3XMvp1kpump";
+export const CONTRACT_ADDRESS = "";
+export const CONTRACT_ADDRESS_SHORT = "";
 
 // Type for social link keys
 export type SocialLinkKey = keyof typeof SOCIAL_LINKS;
